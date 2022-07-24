b5c5724b8e8c3ae4d3f4e351ce6c7d541906f137
Juan Rodenas <95724863+JuanRodenas@users.noreply.github.com>
2022-07-24 21:24:36 +0200

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -5,7 +5,7 @@
 # Buscamos y actualizamos sistema
 sudo apt update && sudo apt upgrade -y > /dev/null 2>&1
 # Descarga e instala las últimas versiones de paquetes y realiza la actualización pasando a la próxima versión
-sudo apt-get dist-upgrade -y && sudo apt-get full-upgrade -y > /dev/null 2>&1
+sudo apt-get full-upgrade -y > /dev/null 2>&1
 # Elimino software y librerias innecesarias
 sudo apt-get -y autoremove && sudo apt clean > /dev/null 2>&1
 
@@ -15,7 +15,7 @@ ID="YOUR_TOKEN_CHAT"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 DNS="1.1.1.1"
 servidor=$(hostname)
-MSG="\U1F4BE nasacloud actualizada \U2139"
+MSG="\U1F4BE $servidor actualizada \U2139"
 IP="192.168.1.10"
 PUBLIC_IP=$(curl http://checkip.amazonaws.com)
 mensaje="Se ha actualizado el servidor $servidor \U1F504"
