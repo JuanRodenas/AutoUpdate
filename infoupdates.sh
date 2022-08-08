9546da9fc9f74ab8460afcb8384f4881b3cc777f
Juan Rodenas <95724863+JuanRodenas@users.noreply.github.com>
2022-08-08 10:00:17 +0200

    Update infoupdates.sh

--- infoupdates.sh
+++ infoupdates.sh
@@ -11,10 +11,14 @@ ID="YOUR_TOKEN_CHAT"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 DNS="1.1.1.1"
 servidor=$(hostname)
+uptime=$(uptime -p)
 PUBLIC_IP=$(curl -s -X GET http://checkip.amazonaws.com)
 cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
+FREERAM=$(free -mh | grep Mem | awk '{print $4}')
+USEDRAM=$(free -mh | grep Mem | awk '{print $3}')
 actualizaciones=$(/usr/bin/apt-get -s -q -u upgrade | grep -v '\.\.\.' | grep -v ':' | grep -v 'Inst ' | grep -v 'Conf ')
-mensaje="Se encuentran disponibles las siguientes actualizaciones para el servidor $servidor:\n\n$actualizaciones"
+mensaje="\U267B Actualizaciones disponibles para el servidor $servidor:\n\n$actualizaciones"
+MSG="\U1F353 $servidor informa \U1F6A8"
 
 # Envío del mensaje
 /usr/bin/ping -c2 $DNS > /dev/null 2>&1
@@ -25,7 +29,7 @@ else
         curl -s -X POST $URL \
         -d chat_id=$ID \
         -d parse_mode=HTML \
-        -d text="$(printf "$MSG\n\t\t\U2B50 Host: <code>$servidor</code>\t\U2B50\n\t\t- CPU temp: <code>$((cpu/1000))°</code>\n\t\t- IP pública: <code>$PUBLIC_IP</code>\n\t\t- Mensaje: <code>$mensaje</code>")" \
+        -d text="$(printf "$MSG<code>\n\t\t- Host: $servidor\n\t\t- Uptime: $UPTIME\n\t\t- CPU temp: $((cpu/1000))°\n\t\t- RAM: Used $USEDRAM/ Free $FREERAM\n\t\t- IP pública: $PUBLIC_IP\n\t\t- Mensaje: $mensaje</code>")" \
 > /dev/null 2>&1
         exit 0
 fi
